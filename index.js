64f30d3f391b67719979cb3a0cc148b02da23ca9
ARNAB CHAULYA <139354385+arnab1616@users.noreply.github.com>
2024-05-04 21:41:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.get('/', (req,res)=>{
     res.json({message:"Deploy succesfull!"})
 })
-app.get('api/fetch/location',(req, res)=>{
+app.get('/api/fetch/location',(req, res)=>{
     res.json({message:"User location comming out, Please wait !"});
 })
 app.listen(port,()=>{

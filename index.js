2df730ea1d2b1de5b9f4b51c62ca022313fa67bf
ARNAB CHAULYA <139354385+arnab1616@users.noreply.github.com>
2024-05-05 03:01:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,36 +30,25 @@ app.get('/api/fetch/location', async(req, res)=>{
         const data = await response.json();
         const response1 = await fetch(`https://ipapi.co/${data.ip}/json/`);
         const userData = await response1.json();
-        pool.getConnection((err, connection) => {
-          if (err) {
-            console.error('Error getting MySQL connection:', err);
-            return;
-          }
+        // pool.getConnection((err, connection) => {
+        //   if (err) {
+        //     console.error('Error getting MySQL connection:', err);
+        //     return;
+        //   }
         
-          connection.query(`INSERT INTO user_geolocation (ip_address,network,city,region,country,postal_code,latitude,longitude) VALUES('${userData.ip}','${userData.network}','${userData.city}','${userData.region}','${userData.country_name}','${userData.postal}','${userData.latitude}','${userData.longitude}')`, (queryErr, results) => {
-            connection.release(); // Release the connection back to the pool after use
+        //   connection.query(`INSERT INTO user_geolocation (ip_address,network,city,region,country,postal_code,latitude,longitude) VALUES('${userData.ip}','${userData.network}','${userData.city}','${userData.region}','${userData.country_name}','${userData.postal}','${userData.latitude}','${userData.longitude}')`, (queryErr, results) => {
+        //     connection.release(); 
         
-            if (queryErr) {
-              console.error('Error executing query:', queryErr);
-              res.json({error: queryErr.message})
-            } else {
-              console.log('Query result:', results);
-                res.json(results)
-            }
-          });
-        });
-        // pool.query(`INSERT INTO user_geolocation (ip_address,network,city,region,country,postal_code,latitude,longitude) 
-        //             VALUES('${userData.ip}','${userData.network}','${userData.city}','${userData.region}','${userData.country_name}','${userData.postal}','${userData.latitude}','${userData.longitude}')`, 
-        //     (err, results) => {
-        //         if (err) {
-        //         console.error('Error executing query:', err);
-        //         } else {
-        //         console.log('Query result:', results);
+        //     if (queryErr) {
+        //       console.error('Error executing query:', queryErr);
+        //       res.json({error: queryErr.message})
+        //     } else {
+        //       console.log('Query result:', results);
         //         res.json(results)
-        //         }
-        //         pool.end();
+        //     }
+        //   });
         // });
-        // res.json(userData)
+        res.json(userData);
     }catch(err){
         console.log(err.message);
     }

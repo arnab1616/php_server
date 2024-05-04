8ccfeae5f48de64cc354af1338c46a66b92b31dc
ARNAB CHAULYA <139354385+arnab1616@users.noreply.github.com>
2024-05-05 02:55:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,28 +24,46 @@ app.get('/', (req,res)=>{
       });
     });
 })
-// app.get('/api/fetch/location', async(req, res)=>{
-//     try{
-//         const response = await fetch('https://api.ipify.org?format=json');
-//         const data = await response.json();
-//         const response1 = await fetch(`https://ipapi.co/${data.ip}/json/`);
-//         const userData = await response1.json();
-//         pool.query(`INSERT INTO user_geolocation (ip_address,network,city,region,country,postal_code,latitude,longitude) 
-//                     VALUES('${userData.ip}','${userData.network}','${userData.city}','${userData.region}','${userData.country_name}','${userData.postal}','${userData.latitude}','${userData.longitude}')`, 
-//             (err, results) => {
-//                 if (err) {
-//                 console.error('Error executing query:', err);
-//                 } else {
-//                 console.log('Query result:', results);
-//                 res.json(results)
-//                 }
-//                 pool.end();
-//         });
-//         // res.json(userData)
-//     }catch(err){
-//         console.log(err.message);
-//     }
-// })
+app.get('/api/fetch/location', async(req, res)=>{
+    try{
+        const response = await fetch('https://api.ipify.org?format=json');
+        const data = await response.json();
+        const response1 = await fetch(`https://ipapi.co/${data.ip}/json/`);
+        const userData = await response1.json();
+        pool.getConnection((err, connection) => {
+          if (err) {
+            console.error('Error getting MySQL connection:', err);
+            return;
+          }
+        
+          connection.query(`INSERT INTO user_geolocation (ip_address,network,city,region,country,postal_code,latitude,longitude) VALUES('${userData.ip}','${userData.network}','${userData.city}','${userData.region}','${userData.country_name}','${userData.postal}','${userData.latitude}','${userData.longitude}')`, (queryErr, results) => {
+            connection.release(); // Release the connection back to the pool after use
+        
+            if (queryErr) {
+              console.error('Error executing query:', queryErr);
+              res.json({error: queryErr.message})
+            } else {
+              console.log('Query result:', results);
+                res.json(results)
+            }
+          });
+        });
+        // pool.query(`INSERT INTO user_geolocation (ip_address,network,city,region,country,postal_code,latitude,longitude) 
+        //             VALUES('${userData.ip}','${userData.network}','${userData.city}','${userData.region}','${userData.country_name}','${userData.postal}','${userData.latitude}','${userData.longitude}')`, 
+        //     (err, results) => {
+        //         if (err) {
+        //         console.error('Error executing query:', err);
+        //         } else {
+        //         console.log('Query result:', results);
+        //         res.json(results)
+        //         }
+        //         pool.end();
+        // });
+        // res.json(userData)
+    }catch(err){
+        console.log(err.message);
+    }
+})
 app.listen(port,()=>{
     console.log("Server running on port -> ", port);
 })

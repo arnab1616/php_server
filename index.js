243dc05f956374e1617c63ecfdb1e4b905d2582c
ARNAB CHAULYA <139354385+arnab1616@users.noreply.github.com>
2024-05-04 22:05:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,28 +11,7 @@ const port = 3000;
   })
 
 app.get('/', (req,res)=>{
-    connection.connect(function(err){
-    if(err){
-      console.log(err.code)
-      console.log(err.fatal)
-    } else{
-        console.log("phpMyAdmin connected successfull!");
-    }
-  })
-  $query = 'SELECT * FROM xyz';
-
-    connection.query($query, function(err, rows, fields){
-    if(err){
-      console.log("An error ocoure");
-      return;
-    }
-    console.log("Qurey succesfull! ", rows);
-      res.json(rows);
-  })
-    connection.end(function(){
-    console.log("Connection closed");
-  })
-    // res.json({message:"Deploy succesfull!"})
+    res.json({message:"Deploy succesfull!"})
 })
 app.get('/api/fetch/location',(req, res)=>{
     res.json({message:"User location comming out, Please wait !"});

005b4499b14e8dad7537995e7138def538582692
ARNAB CHAULYA <139354385+arnab1616@users.noreply.github.com>
2024-05-06 15:10:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
 const express = require('express')
 const pool = require('./phpDB.js')
+const axios = require('axios')
 
 const app = express();
 const port = 3000;
 // const 
-app.get('/', (req,res)=>{
-    pool.getConnection((err, connection) => {
-      if (err) {
-        console.error('Error getting MySQL connection:', err);
-        return;
-      }
-    
-      connection.query('SELECT * FROM user', (queryErr, results) => {
-        connection.release(); // Release the connection back to the pool after use
-    
-        if (queryErr) {
-          console.error('Error executing query:', queryErr);
-          res.json({error: queryErr.message})
-        } else {
-          console.log('Query result:', results);
-            res.json(results)
+app.get('/', async (req,res)=>{
+    const options = {
+        method: 'GET',
+        url: 'https://find-any-ip-address-or-domain-location-world-wide.p.rapidapi.com/iplocation',
+        params: {
+          apikey: '873dbe322aea47f89dcf729dcc8f60e8'
+        },
+        headers: {
+              'X-RapidAPI-Key': 'f5477797a3mshfb6292822327c3bp18707ajsn5075c83ad6e0',
+              'X-RapidAPI-Host': 'find-any-ip-address-or-domain-location-world-wide.p.rapidapi.com'
         }
-      });
-    });
+      };
+    try {
+        const response = await axios.request(options);
+        res.json(response.data);
+        console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+    // pool.getConnection((err, connection) => {
+    //   if (err) {
+    //     console.error('Error getting MySQL connection:', err);
+    //     return;
+    //   }
+    
+    //   connection.query('SELECT * FROM user', (queryErr, results) => {
+    //     connection.release(); // Release the connection back to the pool after use
+    
+    //     if (queryErr) {
+    //       console.error('Error executing query:', queryErr);
+    //       res.json({error: queryErr.message})
+    //     } else {
+    //       console.log('Query result:', results);
+    //         res.json(results)
+    //     }
+    //   });
+    // });
 })
 app.get('/api/fetch/location', async(req, res)=>{
     try{
